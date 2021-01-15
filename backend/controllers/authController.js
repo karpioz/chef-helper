449ee3c1ada4d8a50b7152b3bb45a9248194bd69
karpioz <karpioz@gmail.com>
2021-01-15 22:16:58 +0000

    sendgrid email auth added with errors

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,11 @@
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import sendGridMail from "@sendgrid/mail";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+sendGridMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // @desc Register new user
 // @route POST /api/signup/
@@ -35,4 +42,50 @@ const signupUser = (req, res) => {
   });
 };
 
-export { signupUser };
+const signupUserWithSendGrid = (req, res) => {
+  // geting data from request body
+  const { name, email, password } = req.body;
+
+  // checking database for existing email
+  User.findOne({ email }).exec((err, user) => {
+    if (user) {
+      return res.status(400).json({
+        error: "Email is taken",
+      });
+    }
+  });
+
+  // creating token for email activation
+  const token = jwt.sign(
+    { name, email, password },
+    process.env.JWT_ACCOUNT_ACTIVATION,
+    { expiresIn: "10m" }
+  );
+
+  // creating activating email
+  const emailData = {
+    from: process.env.EMAIL_FROM,
+    to: email,
+    subject: "Account activation link",
+    html: `
+    <h1>Chef Helper account activation</h1>
+    <p>Please use this link to activate your account</p>
+    <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
+    <p>${process.env.CLIENT_URL}</p>
+    `,
+  };
+
+  // sending an email
+  sendGridMail
+    .send(emailData)
+    .then((sent) => {
+      return res.json({
+        message: `Activation email has been sent to ${email}. Activation link will expire in 10 minutes`,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export { signupUser, signupUserWithSendGrid };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,13 +4,21 @@ import express from "express";
 const router = express.Router();
 
 // importing controllers
-import { signupUser } from "../controllers/authController.js";
+import {
+  signupUser,
+  signupUserWithSendGrid,
+} from "../controllers/authController.js";
 
 // importing validators
 import { userSignUpValidator } from "../validators/authValidator.js";
 import { runValidation } from "../validators/index.js";
 
 // routes
-router.post("/signup", userSignUpValidator, runValidation, signupUser);
+router.post(
+  "/signup",
+  userSignUpValidator,
+  runValidation,
+  signupUserWithSendGrid
+);
 
 export default router;
