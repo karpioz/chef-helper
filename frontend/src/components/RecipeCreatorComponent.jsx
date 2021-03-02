e44ead609404fe5650781798b4581b4e1a75418b
Pawel Karpinski <karpioz@gmail.com>
2021-03-02 23:45:13 +0000

    healthLabels stored as array fixed

--- frontend/src/components/RecipeCreatorComponent.jsx
+++ frontend/src/components/RecipeCreatorComponent.jsx
@@ -1,41 +1,62 @@
-import React from "react";
-import { Form, Button, Col } from "react-bootstrap";
+import React from 'react'
+import { Form, Button, Col } from 'react-bootstrap'
 
 const RecipeCreatorComponent = ({
-  handleRecipeSubmit,
-  handleRecipeCreatorInputChange,
-  recipeCreatorData,
+	handleRecipeSubmit,
+	handleRecipeCreatorInputChange,
+	recipeCreatorData
 }) => {
-  const { label, healthLabels } = recipeCreatorData;
+	const { label, healthLabels, image } = recipeCreatorData
 
-  return (
-    <Form onSubmit={handleRecipeSubmit}>
-      <Form.Group controlId="label">
-        <Form.Label>Dish Name</Form.Label>
-        <Form.Control
-          type="text"
-          onChange={handleRecipeCreatorInputChange("label")}
-          placeholder="Dish Name"
-          name="label"
-          value={label}
-        ></Form.Control>
-      </Form.Group>
-      <Form.Group controlId="healthLabels">
-        <Form.Label>Health Labels</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Health labels, separate by coma ','"
-          onChange={handleRecipeCreatorInputChange("healthLabels")}
-          name="healthLabels"
-          value={healthLabels}
-        ></Form.Control>
-      </Form.Group>
+	return (
+		<Form onSubmit={handleRecipeSubmit}>
+			<Form.Row>
+				<Form.Group as={Col} controlId='label'>
+					<Form.Label>Dish Name</Form.Label>
+					<Form.Control
+						type='text'
+						onChange={handleRecipeCreatorInputChange('label')}
+						placeholder='Dish Name'
+						name='label'
+						value={label}
+					></Form.Control>
+				</Form.Group>
+				<Form.Group as={Col} controlId='healthLabels'>
+					<Form.Label>Health Labels</Form.Label>
+					<Form.Control
+						type='text'
+						placeholder="Health labels, separate by coma ','"
+						onChange={handleRecipeCreatorInputChange('healthLabels')}
+						name='healthLabels'
+						value={healthLabels}
+					></Form.Control>
+				</Form.Group>
+			</Form.Row>
+			<Form.Row>
+				<Form.Group as={Col} controlId='image'>
+					<Form.Label>Image Link</Form.Label>
+					<Form.Control
+						type='text'
+						placeholder='Please paste url for image file'
+						onChange={handleRecipeCreatorInputChange('image')}
+						name='image'
+						value={image}
+					></Form.Control>
+				</Form.Group>
+				<Form.Group as={Col} controlId='photo'>
+					<Form.Label>Photo Upload</Form.Label>
+					<Form.Control
+						type='file'
+						onChange={handleRecipeCreatorInputChange('image')}
+						name='image'
+					></Form.Control>
+				</Form.Group>
+			</Form.Row>
+			<Button variant='success' type='submit'>
+				Add Recipe
+			</Button>
+		</Form>
+	)
+}
 
-      <Button variant="success" type="submit">
-        Add Recipe
-      </Button>
-    </Form>
-  );
-};
-
-export default RecipeCreatorComponent;
+export default RecipeCreatorComponent
--- frontend/src/screens/AdminScreen.jsx
+++ frontend/src/screens/AdminScreen.jsx
@@ -1,303 +1,307 @@
-import React, { useState, useEffect } from "react";
-import { Row, Col, Form, Button } from "react-bootstrap";
-import axios from "axios";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.min.css";
-import RecipeCreatorComponent from "../components/RecipeCreatorComponent";
+import React, { useState, useEffect } from 'react'
+import { Row, Col, Form, Button } from 'react-bootstrap'
+import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.min.css'
+import RecipeCreatorComponent from '../components/RecipeCreatorComponent'
 
 const AdminScreen = () => {
-  // state with useState hook
-  const [taskPriority, setTaskPriority] = useState("select");
-  const [taskFormData, setTaskFormData] = useState({
-    taskName: "",
-    assignedTo: "",
-    priority: "low",
-  });
-  const [users, setUsers] = useState([]);
-  const [productsFormData, setProductsFormData] = useState({
-    name: "",
-    countInStock: "",
-    price: "",
-    defaultWeightInGrams: "",
-  });
-  const [recipeCreatorData, setRecipeCreatorData] = useState({
-    label: "",
-    healthLabels: [],
-  });
-  const [recipeCreatorDataPrepared, setRecipeCreatorDataPrepared] = useState({
-    label: "smth",
-    healthLabels: [""],
-  });
-  // destructuring state
-  const { name, countInStock, price, defaultWeightInGrams } = productsFormData;
-  const { taskName, assignedTo, priority } = taskFormData;
+	// state with useState hook
+	const [taskPriority, setTaskPriority] = useState('select')
+	const [taskFormData, setTaskFormData] = useState({
+		taskName: '',
+		assignedTo: '',
+		priority: 'low'
+	})
+	const [users, setUsers] = useState([])
+	const [productsFormData, setProductsFormData] = useState({
+		name: '',
+		countInStock: '',
+		price: '',
+		defaultWeightInGrams: ''
+	})
+	const [recipeCreatorData, setRecipeCreatorData] = useState({
+		label: '',
+		healthLabels: [],
+		image: ''
+	})
 
-  // handling Product form changes
-  const handleProductInputChange = (name) => (event) => {
-    console.log(event.target.value);
-    // getting existing state and update the key with same name as function argument
-    setProductsFormData({ ...productsFormData, [name]: event.target.value });
-  };
-  // handling Tasks form changes
-  const handleTaskInputChange = (name) => (event) => {
-    console.log(event.target.value);
-    // getting existing state and update the key with same name as function argument
-    setTaskFormData({ ...taskFormData, [name]: event.target.value });
-  };
+	// destructuring state
+	const { name, countInStock, price, defaultWeightInGrams } = productsFormData
+	const { taskName, assignedTo, priority } = taskFormData
 
-  // submiting the Product form
-  const handleProductSubmit = (event) => {
-    event.preventDefault();
-    setProductsFormData({ ...productsFormData });
-    // axios to connect with backend
-    axios({
-      method: "POST",
-      url: `${process.env.REACT_APP_API}/products/`,
-      data: { name, countInStock, price, defaultWeightInGrams },
-    })
-      .then((response) => {
-        setProductsFormData({
-          ...productsFormData,
-          name: "",
-          countInStock: "",
-          price: "",
-          defaultWeightInGrams: "",
-        });
-        toast.success(response.data.message);
-      })
-      .catch((error) => {
-        console.log("PRODUCT CREATE ERROR", error.response.data);
-        setProductsFormData({ ...productsFormData });
-        toast.error(error.response.data.error);
-      });
-  };
+	// handling Product form changes
+	const handleProductInputChange = name => event => {
+		console.log(event.target.value)
+		// getting existing state and update the key with same name as function argument
+		setProductsFormData({ ...productsFormData, [name]: event.target.value })
+	}
+	// handling Tasks form changes
+	const handleTaskInputChange = name => event => {
+		console.log(event.target.value)
+		// getting existing state and update the key with same name as function argument
+		setTaskFormData({ ...taskFormData, [name]: event.target.value })
+	}
 
-  // Tasks Form Handling
-  // Priority Colour Styles
-  const setPriorityColour = (priority) => {
-    let colour;
-    switch (priority) {
-      case "low":
-        colour = "info";
-        break;
-      case "medium":
-        colour = "warning";
-        break;
-      case "high":
-        colour = "danger";
-        break;
-      default:
-        colour = "success";
-    }
-    return colour;
-  };
+	// submiting the Product form
+	const handleProductSubmit = event => {
+		event.preventDefault()
+		setProductsFormData({ ...productsFormData })
+		// axios to connect with backend
+		axios({
+			method: 'POST',
+			url: `${process.env.REACT_APP_API}/products/`,
+			data: { name, countInStock, price, defaultWeightInGrams }
+		})
+			.then(response => {
+				setProductsFormData({
+					...productsFormData,
+					name: '',
+					countInStock: '',
+					price: '',
+					defaultWeightInGrams: ''
+				})
+				toast.success(response.data.message)
+			})
+			.catch(error => {
+				console.log('PRODUCT CREATE ERROR', error.response.data)
+				setProductsFormData({ ...productsFormData })
+				toast.error(error.response.data.error)
+			})
+	}
 
-  const changePriority = (event) => {
-    setTaskPriority(event.target.value);
-    setTaskFormData({ ...taskFormData, priority: event.target.value });
-  };
+	// Tasks Form Handling
+	// Priority Colour Styles
+	const setPriorityColour = priority => {
+		let colour
+		switch (priority) {
+			case 'low':
+				colour = 'info'
+				break
+			case 'medium':
+				colour = 'warning'
+				break
+			case 'high':
+				colour = 'danger'
+				break
+			default:
+				colour = 'success'
+		}
+		return colour
+	}
 
-  const changeAssignedTo = (event) => {
-    setTaskFormData({ ...taskFormData, assignedTo: event.target.value });
-  };
+	const changePriority = event => {
+		setTaskPriority(event.target.value)
+		setTaskFormData({ ...taskFormData, priority: event.target.value })
+	}
 
-  const handleTaskSubmit = (event) => {
-    event.preventDefault();
-    setTaskFormData({ ...taskFormData, name: taskName });
-    // axios to connect with backend
-    axios({
-      method: "POST",
-      url: `${process.env.REACT_APP_API}/tasks`,
-      data: { taskName, assignedTo, priority },
-    })
-      .then((response) => {
-        setTaskFormData({
-          ...taskFormData,
-          taskName: "",
-          assignedTo: "",
-          priority: "",
-        });
-        toast.success(response.data.message);
-      })
-      .catch((error) => {
-        console.log("TASK CREATE ERROR", error.response.data);
-        setProductsFormData({ ...productsFormData });
-        toast.error(error.response.data.error);
-      });
-  };
-  // converting inputs to array
-  const changeToArray = () => {
-    let healthLabelsArr = [];
+	const changeAssignedTo = event => {
+		setTaskFormData({ ...taskFormData, assignedTo: event.target.value })
+	}
 
-    if (recipeCreatorData) {
-      healthLabelsArr = [...recipeCreatorData.healthLabels];
-    }
+	const handleTaskSubmit = event => {
+		event.preventDefault()
+		setTaskFormData({ ...taskFormData, name: taskName })
+		// axios to connect with backend
+		axios({
+			method: 'POST',
+			url: `${process.env.REACT_APP_API}/tasks`,
+			data: { taskName, assignedTo, priority }
+		})
+			.then(response => {
+				setTaskFormData({
+					...taskFormData,
+					taskName: '',
+					assignedTo: '',
+					priority: ''
+				})
+				toast.success(response.data.message)
+			})
+			.catch(error => {
+				console.log('TASK CREATE ERROR', error.response.data)
+				setProductsFormData({ ...productsFormData })
+				toast.error(error.response.data.error)
+			})
+	}
+	/* // converting inputs to array
+	const changeToArray = () => {
+		let healthLabelsArr = []
 
-    setRecipeCreatorData({
-      ...recipeCreatorData,
-      healthLabels: healthLabelsArr,
-    });
+		if (recipeCreatorData) {
+			healthLabelsArr = [...recipeCreatorData.healthLabels]
+		}
 
-    console.log("data should be modified:" + recipeCreatorData.healthLabels);
-  };
+		setRecipeCreatorData({
+			...recipeCreatorData,
+			healthLabels: healthLabelsArr
+		})
 
-  // handling recipe creator
-  const handleRecipeSubmit = (e) => {
-    e.preventDefault();
-    changeToArray();
-    console.log("Recipe submitted");
-  };
+		console.log('data should be modified:' + recipeCreatorData.healthLabels)
+	} */
 
-  const handleRecipeCreatorInputChange = (name) => (event) => {
-    // getting existing state and update the key with same name as function argument
-    setRecipeCreatorData({
-      ...recipeCreatorData,
-      [name]: event.target.value,
-    });
-  };
+	// handling recipe creator
+	const handleRecipeSubmit = e => {
+		e.preventDefault()
+		console.log(recipeCreatorData)
+		console.log('Recipe submitted')
+	}
 
-  // fetching users on load
-  const fetchUsers = async () => {
-    const response = await axios.get(
-      `${process.env.REACT_APP_API}/users/names`
-    );
-    //console.log(response);
-    if (response) {
-      setUsers(response.data);
-    } else {
-      console.log("something went wrong when fetching users...");
-    }
-  };
+	const handleRecipeCreatorInputChange = name => event => {
+		// getting existing state and update the key with same name as function argument
+		if (name === 'healthLabels') {
+			let healthLabArr = event.target.value.split(',')
+			setRecipeCreatorData({
+				...recipeCreatorData,
+				healthLabels: healthLabArr
+			})
+		} else {
+			setRecipeCreatorData({
+				...recipeCreatorData,
+				[name]: event.target.value
+			})
+		}
+	}
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+	// fetching users on load
+	const fetchUsers = async () => {
+		const response = await axios.get(`${process.env.REACT_APP_API}/users/names`)
+		//console.log(response);
+		if (response) {
+			setUsers(response.data)
+		} else {
+			console.log('something went wrong when fetching users...')
+		}
+	}
 
-  return (
-    <>
-      <Row>
-        <ToastContainer />
-        <Col>
-          <h1 className="text-center my-3">Admin Dashboard</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h2>Pantry Management</h2>
-          <Form onSubmit={handleProductSubmit}>
-            <Form.Group controlId="Name">
-              <Form.Label>Product Name</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Product Name"
-                onChange={handleProductInputChange("name")}
-                value={name}
-              ></Form.Control>
-            </Form.Group>
-            <Form.Group controlId="quantity">
-              <Form.Label>Quantity</Form.Label>
-              <Form.Control
-                type="number"
-                step="0.01"
-                placeholder="Quantity"
-                onChange={handleProductInputChange("countInStock")}
-                value={countInStock}
-              ></Form.Control>
-            </Form.Group>
-            <Form.Group controlId="price">
-              <Form.Label>Price</Form.Label>
-              <Form.Control
-                type="number"
-                step="0.01"
-                placeholder="Price"
-                onChange={handleProductInputChange("price")}
-                value={price}
-              ></Form.Control>
-            </Form.Group>
-            <Form.Group controlId="weight">
-              <Form.Label>Weight</Form.Label>
-              <Form.Control
-                type="number"
-                step="0.01"
-                placeholder="Weight"
-                onChange={handleProductInputChange("defaultWeightInGrams")}
-                value={defaultWeightInGrams}
-              ></Form.Control>
-            </Form.Group>
+	useEffect(() => {
+		fetchUsers()
+	}, [])
 
-            <Button type="submit" variant="success">
-              Add Product
-            </Button>
-          </Form>
-        </Col>
-        <Col>
-          <h2>Task</h2>
-          <Form onSubmit={handleTaskSubmit}>
-            <Form.Group controlId="Name">
-              <Form.Label>Job to do</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Task Name"
-                onChange={handleTaskInputChange("taskName")}
-                value={taskName}
-              ></Form.Control>
-            </Form.Group>
-            <Form.Group controlId="priority">
-              <Form.Label>Priority</Form.Label>
+	return (
+		<>
+			<Row>
+				<ToastContainer />
+				<Col>
+					<h1 className='text-center my-3'>Admin Dashboard</h1>
+				</Col>
+			</Row>
+			<Row>
+				<Col>
+					<h2>Pantry Management</h2>
+					<Form onSubmit={handleProductSubmit}>
+						<Form.Group controlId='Name'>
+							<Form.Label>Product Name</Form.Label>
+							<Form.Control
+								type='text'
+								placeholder='Product Name'
+								onChange={handleProductInputChange('name')}
+								value={name}
+							></Form.Control>
+						</Form.Group>
+						<Form.Group controlId='quantity'>
+							<Form.Label>Quantity</Form.Label>
+							<Form.Control
+								type='number'
+								step='0.01'
+								placeholder='Quantity'
+								onChange={handleProductInputChange('countInStock')}
+								value={countInStock}
+							></Form.Control>
+						</Form.Group>
+						<Form.Group controlId='price'>
+							<Form.Label>Price</Form.Label>
+							<Form.Control
+								type='number'
+								step='0.01'
+								placeholder='Price'
+								onChange={handleProductInputChange('price')}
+								value={price}
+							></Form.Control>
+						</Form.Group>
+						<Form.Group controlId='weight'>
+							<Form.Label>Weight</Form.Label>
+							<Form.Control
+								type='number'
+								step='0.01'
+								placeholder='Weight'
+								onChange={handleProductInputChange('defaultWeightInGrams')}
+								value={defaultWeightInGrams}
+							></Form.Control>
+						</Form.Group>
 
-              <Form.Control
-                as="select"
-                onChange={changePriority}
-                value={priority}
-                className={`bg-${setPriorityColour(priority)} text-light`}
-              >
-                <option className="bg-info" value="low">
-                  Low
-                </option>
-                <option className="bg-warning" value="medium">
-                  Medium
-                </option>
-                <option className="bg-danger" value="high">
-                  High
-                </option>
-              </Form.Control>
-            </Form.Group>
-            <Form.Group controlId="assignedTo">
-              <Form.Label>Assign Task to:</Form.Label>
-              <Form.Control as="select" onChange={changeAssignedTo}>
-                <option>Select user</option>
-                {users.length !== 0 ? (
-                  users.map((user) => (
-                    <option key={user._id} value={user._id}>
-                      {user.name}
-                    </option>
-                  ))
-                ) : (
-                  <option className="bg-danger text-light">
-                    Error fetching users
-                  </option>
-                )}
-              </Form.Control>
-            </Form.Group>
+						<Button type='submit' variant='success'>
+							Add Product
+						</Button>
+					</Form>
+				</Col>
+				<Col>
+					<h2>Task</h2>
+					<Form onSubmit={handleTaskSubmit}>
+						<Form.Group controlId='Name'>
+							<Form.Label>Job to do</Form.Label>
+							<Form.Control
+								type='text'
+								placeholder='Task Name'
+								onChange={handleTaskInputChange('taskName')}
+								value={taskName}
+							></Form.Control>
+						</Form.Group>
+						<Form.Group controlId='priority'>
+							<Form.Label>Priority</Form.Label>
 
-            <Button type="submit" variant="warning">
-              Add Task
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-      <Row className="my-3">
-        <Col>
-          <h2>Recipe Creator</h2>
-          <RecipeCreatorComponent
-            handleRecipeSubmit={handleRecipeSubmit}
-            handleRecipeCreatorInputChange={handleRecipeCreatorInputChange}
-            recipeCreatorData={recipeCreatorData}
-          />
-        </Col>
-      </Row>
-    </>
-  );
-};
+							<Form.Control
+								as='select'
+								onChange={changePriority}
+								value={priority}
+								className={`bg-${setPriorityColour(priority)} text-light`}
+							>
+								<option className='bg-info' value='low'>
+									Low
+								</option>
+								<option className='bg-warning' value='medium'>
+									Medium
+								</option>
+								<option className='bg-danger' value='high'>
+									High
+								</option>
+							</Form.Control>
+						</Form.Group>
+						<Form.Group controlId='assignedTo'>
+							<Form.Label>Assign Task to:</Form.Label>
+							<Form.Control as='select' onChange={changeAssignedTo}>
+								<option>Select user</option>
+								{users.length !== 0 ? (
+									users.map(user => (
+										<option key={user._id} value={user._id}>
+											{user.name}
+										</option>
+									))
+								) : (
+									<option className='bg-danger text-light'>
+										Error fetching users
+									</option>
+								)}
+							</Form.Control>
+						</Form.Group>
 
-export default AdminScreen;
+						<Button type='submit' variant='warning'>
+							Add Task
+						</Button>
+					</Form>
+				</Col>
+			</Row>
+			<Row className='my-3'>
+				<Col>
+					<h2>Recipe Creator</h2>
+					<RecipeCreatorComponent
+						handleRecipeSubmit={handleRecipeSubmit}
+						handleRecipeCreatorInputChange={handleRecipeCreatorInputChange}
+						recipeCreatorData={recipeCreatorData}
+					/>
+				</Col>
+			</Row>
+		</>
+	)
+}
+
+export default AdminScreen
