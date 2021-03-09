3240052e96e919d298d76e026578d01db5e9c747
Pawel Karpinski <karpioz@gmail.com>
2021-03-09 14:31:43 +0000

    working on rota creator

--- frontend/src/components/RotaCreatorComponent.jsx
+++ frontend/src/components/RotaCreatorComponent.jsx
@@ -1,18 +1,53 @@
-import React from "react";
-import { Form } from "react-bootstrap";
+import React, { useState, useEffect } from 'react'
+import { Form } from 'react-bootstrap'
+import moment from 'moment'
+
+moment.locale('en-gb')
 
 const RotaCreatorComponent = () => {
-  return (
-    <Form>
-      <Form.Group>
-        <Form.Label>Select week ending</Form.Label>
-        <Form.Control
-          type="date"
-          onChange={(e) => console.log(e.target.value)}
-        ></Form.Control>
-      </Form.Group>
-    </Form>
-  );
-};
+	const [rota, setRota] = useState([])
 
-export default RotaCreatorComponent;
+	const handleWeekEndingDate = e => {
+		let weekStartingDate = e.target.value
+		let weekDays = [
+			'Monday',
+			'Tuesday',
+			'Wednesday',
+			'Thursday',
+			'Friday',
+			'Saturday',
+			'Sunday'
+		]
+		let newRota = []
+
+		for (let i = 0; i <= 6; i++) {
+			let day = { day: '', date: '' }
+			day.day = weekDays[i]
+			day.date = moment(weekStartingDate).add(i, 'days').format('LLLL')
+			console.log(day)
+			newRota.push(day)
+		}
+
+		setRota(newRota)
+
+		console.log(rota)
+	}
+
+	useEffect(() => {
+		console.log(rota)
+	}, [rota])
+
+	return (
+		<Form>
+			<Form.Group>
+				<Form.Label>Select week ending</Form.Label>
+				<Form.Control
+					type='date'
+					onChange={handleWeekEndingDate}
+				></Form.Control>
+			</Form.Group>
+		</Form>
+	)
+}
+
+export default RotaCreatorComponent
